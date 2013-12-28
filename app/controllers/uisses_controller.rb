62e184a05c9370751a733b2455381b4fccfbe047
Hristo Tanchev <h.tanchev@emerchantpay.com>
2013-12-28 20:02:32 +0200

    removed authentication required

--- app/controllers/uisses_controller.rb
+++ app/controllers/uisses_controller.rb
@@ -1,25 +1,9 @@
 # -*- coding: utf-8 -*-
-
-require 'rake'
-Rake::Task.clear # necessary to avoid tasks being loaded several times in dev mode
-TuSocial::Application.load_tasks
-
 class UissesController < ApplicationController
-
   before_action :set_uiss, only: [:show, :edit, :update, :destroy]
-#  require 'pry'
-  # GET /uisses
-  # GET /uisses.json
   def index
-    @egn = params[:egn]
-    @fac_num = params[:fac_num]
-
-    # puts @egn
-    # test = '9102123046 121210169'
-    # Rake::Task['marks'].invoke(test)
- #   %x["rake marks 9102123046 121210169"]
-#    puts Rake::Task['marks 9102123046 121210169']
-    # @uisses = Uiss.all
+    # @egn = params[:egn]
+    # @fac_num = params[:fac_num]
   end
 
   def testov(first, last)
@@ -56,58 +40,10 @@ class UissesController < ApplicationController
     
     testov(egn, fac_num)
 
-    puts '=====FROM====='
-
-    puts $marks_array.last
-#    puts $marks_array
-    puts '=====TO====='
-
     respond_to do |format|
       format.html { render action: 'index' }
       format.json { $marks_array }
       format.js { render json: $marks_array.to_json }
-      # if @uiss.save
-      #   format.html { redirect_to @uiss, notice: 'Uiss was successfully created.' }
-      #   format.json { render action: 'show', status: :created, location: @uiss }
-      # else
-      #   format.html { render action: 'new' }
-      #   format.json { render json: @uiss.errors, status: :unprocessable_entity }
-      # end
     end
   end
-
-  # # PATCH/PUT /uisses/1
-  # # PATCH/PUT /uisses/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @uiss.update(uiss_params)
-  #       format.html { redirect_to @uiss, notice: 'Uiss was successfully updated.' }
-  #       format.json { head :no_content }
-  #     else
-  #       format.html { render action: 'edit' }
-  #       format.json { render json: @uiss.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # # DELETE /uisses/1
-  # # DELETE /uisses/1.json
-  # def destroy
-  #   @uiss.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to uisses_url }
-  #     format.json { head :no_content }
-  #   end
-  # end
-
-  # private
-  #   # Use callbacks to share common setup or constraints between actions.
-  #   def set_uiss
-  #     @uiss = Uiss.find(params[:id])
-  #   end
-
-  #   # Never trust parameters from the scary internet, only allow the white list through.
-  #   def uiss_params
-  #     params[:uiss]
-  #   end
 end
