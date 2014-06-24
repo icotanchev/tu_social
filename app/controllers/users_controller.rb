1d2fc2a43ecef92124e800bdb19a99917654fc38
Hristo Tanchev <h.tanchev@emerchantpay.com>
2014-06-24 20:12:25 +0300

    Added profile edit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:notice] = 'User was successfully updated.'
+      redirect_to edit_user_url(@user.id)
+    else
+    	flash[:warning] = 'User was not updated!'
+      redirect_to action: :edit
+    end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :pin, :faculty_number, :send_emails, :email)
+	end
+end
