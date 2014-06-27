63da78bc7a34783bb903fb75cfafc75c2a699dbc
Hristo Tanchev <h.tanchev@emerchantpay.com>
2014-06-27 21:56:30 +0300

    Commented resque init

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,13 +1,13 @@
-require 'resque_scheduler/server'
+# require 'resque_scheduler/server'
 
-# Load redis configuration
-redis_config = YAML.load_file(Rails.root.join('config', 'resque.yml'))[Rails.env]
-Resque.redis = Redis.new(
-  host:     redis_config['host'],
-  port:     redis_config['port'],
-  password: redis_config['password'])
-Resque.redis.namespace = 'tu_social'
+# # Load redis configuration
+# redis_config = YAML.load_file(Rails.root.join('config', 'resque.yml'))[Rails.env]
+# Resque.redis = Redis.new(
+#   host:     redis_config['host'],
+#   port:     redis_config['port'],
+#   password: redis_config['password'])
+# Resque.redis.namespace = 'tu_social'
 
-# Load schedule configuration
-Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedules.yml'))
+# # Load schedule configuration
+# Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedules.yml'))
 
